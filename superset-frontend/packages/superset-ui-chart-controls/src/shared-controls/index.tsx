dc23c7ff95eddbb11223323522902399f3168dcb
Fabien <fabien.rassinier@gmail.com>
2022-08-31 16:05:40 +0200

    fix(RB-454): Can't remove Day opt from time grain
    Linked to https://github.com/apache/superset/issues/21268

--- superset-frontend/packages/superset-ui-chart-controls/src/shared-controls/index.tsx
+++ superset-frontend/packages/superset-ui-chart-controls/src/shared-controls/index.tsx
@@ -325,7 +325,6 @@ const granularity_sqla: SharedControlConfig<'SelectControl', ColumnMeta> = {
 const time_grain_sqla: SharedControlConfig<'SelectControl'> = {
   type: 'SelectControl',
   label: TIME_FILTER_LABELS.time_grain_sqla,
-  default: 'P1D',
   description: t(
     'The time granularity for the visualization. This ' +
       'applies a date transformation to alter ' +
@@ -333,9 +332,13 @@ const time_grain_sqla: SharedControlConfig<'SelectControl'> = {
       'The options here are defined on a per database ' +
       'engine basis in the Superset source code.',
   ),
-  mapStateToProps: ({ datasource }) => ({
-    choices: (datasource as Dataset)?.time_grain_sqla || null,
-  }),
+  mapStateToProps: ({ datasource }) => {
+    const choices = (datasource as Dataset)?.time_grain_sqla || null;
+    return {
+      default: choices && choices[0][0],
+      choices,
+    };
+  },
 };
 
 const time_range: SharedControlConfig<'DateFilterControl'> = {
