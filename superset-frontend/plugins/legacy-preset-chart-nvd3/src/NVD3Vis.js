09c59067409466776fdb43dafcbe2d02ef34c752
frassinier <18534166+frassinier@users.noreply.github.com>
2022-11-04 12:52:05 +0100

    fix(RB-593): chart colors must be consistent

--- superset-frontend/plugins/legacy-preset-chart-nvd3/src/NVD3Vis.js
+++ superset-frontend/plugins/legacy-preset-chart-nvd3/src/NVD3Vis.js
@@ -259,7 +259,7 @@ const formatter = getNumberFormatter();
 
 function nvd3Vis(element, props) {
   const {
-    data,
+    data: unsortedData,
     width: maxWidth,
     height: maxHeight,
     annotationData,
@@ -315,6 +315,10 @@ function nvd3Vis(element, props) {
     yIsLogScale,
     sliceId,
   } = props;
+
+  const data = unsortedData.sort((a, b) =>
+    a.key > b.key ? 1 : b.key > a.key ? -1 : 0,
+  );
 
   const isExplore = document.querySelector('#explorer-container') !== null;
   const container = element;
